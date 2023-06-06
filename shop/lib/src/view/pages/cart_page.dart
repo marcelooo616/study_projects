e227d89a343660af52c18bbae7128b6018fab9bf
marcelooo616 <marcelooo2014@hotmail.com>
2023-06-06 15:01:52 -0300

    feat - Construindo componente Carrinho

--- shop/lib/src/view/pages/cart_page.dart
+++ shop/lib/src/view/pages/cart_page.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/src/models/class/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Carrinho'),
       ),
+      body: Column(children: [
+        Card(
+          margin: const EdgeInsets.all(25),
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              Text(
+                'Total:',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(width: 10,),
+              Chip(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  label: Text('R\$${cart.totalAmount}',
+                      style: TextStyle(
+                          color: Theme.of(context)
+                              .primaryTextTheme
+                              .headline6
+                              ?.color))),
+              Spacer(),
+              TextButton(
+                onPressed: (){},
+                child: Text('COMPRA'),
+                style: TextButton.styleFrom(
+                  textStyle: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                  )
+                ),)
+            ]),
+          ),
+        )
+      ]),
     );
   }
 }
