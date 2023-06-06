416690e41881324cb857fa9f999611ffcb78c49d
marcelooo616 <marcelooo2014@hotmail.com>
2023-06-06 16:26:04 -0300

    feat - Itens do Carrinho

--- shop/lib/src/view/pages/cart_page.dart
+++ shop/lib/src/view/pages/cart_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/src/models/class/cart.dart';
+import 'package:shop/src/view/widgets/cart_item.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -10,13 +11,14 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Cart cart = Provider.of(context);
+    final prod = cart.items.values.toList();
     return Scaffold(
       appBar: AppBar(
         title: Text('Carrinho'),
       ),
       body: Column(children: [
         Card(
-          margin: const EdgeInsets.all(25),
+          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
           child: Padding(
             padding: const EdgeInsets.all(10),
             child: Row(
@@ -46,6 +48,12 @@ class CartPage extends StatelessWidget {
                 ),)
             ]),
           ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: prod.length,
+            itemBuilder: (context, index) => CartItemWidget(cartItem: prod[index])
+            )
         )
       ]),
     );
---
+++ shop/lib/src/view/widgets/cart_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shop/src/models/class/cart_item.dart';
+
+class CartItemWidget extends StatelessWidget {
+  final CartItem cartItem;
+
+
+  const CartItemWidget({
+    Key? key,
+    required this.cartItem,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Padding(padding: const EdgeInsets.all(5),
+            child: FittedBox(
+              child: Text('${cartItem.price}'),
+            ) 
+            ,),
+          ),
+          title: Text(cartItem.name!),
+          subtitle: Text('Total: R\$ ${cartItem.price! * cartItem.quantity!}'),
+          trailing: Text('${cartItem.quantity}x'),
+        ),
+      ),
+    );
+  }
+}
