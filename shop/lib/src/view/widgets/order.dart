b4ec20cd597cc3acce13fbf2ba58dba8c12a02b7
marcelooo616 <marcelooo2014@hotmail.com>
2023-06-06 22:16:49 -0300

    feat - Componente pedidos#03

--- shop/lib/src/view/widgets/order.dart
+++ shop/lib/src/view/widgets/order.dart
@@ -3,29 +3,64 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:intl/intl.dart';
 import 'package:shop/src/models/class/order.dart';
+import 'package:shop/src/models/class/product.dart';
 
-class OrderWidget extends StatelessWidget {
-
+class OrderWidget extends StatefulWidget {
   final Order order;
 
+  const OrderWidget({Key? key, required this.order}) : super(key: key);
 
-  const OrderWidget({
-    Key? key,
-    required this.order
-    }) : super(key: key);
+  @override
+  State<OrderWidget> createState() => _OrderWidgetState();
+}
 
+class _OrderWidgetState extends State<OrderWidget> {
+  bool _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Text('R\$ ${order.total.toStringAsFixed(2)}'),
-        subtitle: Text(
-          DateFormat('dd/MM/yy hh:mm').format(order.date)
-          ),
-        trailing: IconButton(
-          icon: Icon(Icons.expand_more),
-          onPressed: (){},
-          )
+      child: Column(
+        children: [
+          ListTile(
+              title: Text('R\$ ${widget.order.total.toStringAsFixed(2)}'),
+              subtitle: Text(DateFormat('dd/MM/yy hh:mm').format(widget.order.date)),
+              trailing: IconButton(
+                icon: Icon(Icons.expand_more),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              )),
+              if(_expanded)
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                height: widget.order.products.length * 40.0,
+                child: ListView(
+                  children: widget.order.products.map(
+                    (product) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(product.name.toString(), 
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          ),
+                          ),
+                          Text('${product.quantity}x R\$ ${product.price}',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey
+                          ),
+                          )
+                        ],
+                      );
+                    },
+                  ).toList(),
+                ),
+              )
+        ],
       ),
     );
   }
