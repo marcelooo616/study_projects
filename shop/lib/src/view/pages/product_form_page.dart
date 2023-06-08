5217c7e2112d3cb94db8350c71fad3993fd68de6
marcelooo616 <marcelooo2014@hotmail.com>
2023-06-08 19:02:30 -0300

    feat - Campo de entrada de URL

--- shop/lib/src/view/pages/product_form_page.dart
+++ shop/lib/src/view/pages/product_form_page.dart
@@ -10,13 +10,31 @@ class ProductFormPage extends StatefulWidget {
 class _ProductFormPageState extends State<ProductFormPage> {
   final _priceFocus = FocusNode();
   final _descriptionFocus = FocusNode();
-
+  final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
+  
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocus.addListener(updateImage);
+  }
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
     _priceFocus.dispose();
+    _descriptionFocus.dispose();
+    _imageUrlFocus.removeListener(updateImage);
+    _imageUrlFocus.dispose();
+
   }
+
+  void updateImage(){
+    setState(() {
+       
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -51,7 +69,43 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 focusNode: _descriptionFocus,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+                onFieldSubmitted: (_){
+                  FocusScope.of(context).requestFocus(_imageUrlFocus);
+                },
 
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _imageUrlController,
+                      decoration: InputDecoration(labelText: 'Url da Imagem'),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.url,
+                      focusNode: _imageUrlFocus,
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: const EdgeInsets.only(top: 10, left: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(
+                        color: Colors.grey
+                      )
+                    ),
+                    alignment: Alignment.center,
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Informe a Url')
+                        : FittedBox(
+                      child: Image.network(_imageUrlController.text),
+                      fit: BoxFit.cover,
+                    )
+                  )
+                  
+                ],
               ),
             ],
           ),
