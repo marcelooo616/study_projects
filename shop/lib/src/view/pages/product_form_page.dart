180d7ec4820df104450addcc1abf5780a24a9ade
marcelooo616 <marcelooo2014@hotmail.com>
2023-06-08 21:28:17 -0300

    feat - Validação do formulario#01

--- shop/lib/src/view/pages/product_form_page.dart
+++ shop/lib/src/view/pages/product_form_page.dart
@@ -43,6 +43,11 @@ class _ProductFormPageState extends State<ProductFormPage> {
   }
 
   void _submitForm(){
+    final isValid = _formKey.currentState?.validate() ?? false;
+
+    if(!isValid){
+      return;
+    }
     _formKey.currentState?.save();
     final newProduct = Product(
         id: Random().nextDouble().toString(),
@@ -52,7 +57,7 @@ class _ProductFormPageState extends State<ProductFormPage> {
         imageUrl: _formData['imageUrl'] as String,
     );
 
-    
+
   }
 
   @override
@@ -78,6 +83,16 @@ class _ProductFormPageState extends State<ProductFormPage> {
                   FocusScope.of(context).requestFocus(_priceFocus);
                 },
                 onSaved: (name) => _formData['name'] = name ?? '',
+                validator: (_name){
+                  final name = _name ?? '';
+                  if(name.trim().isEmpty){
+                    return 'Nome e obrigatorio';
+                  }
+                  if(name.trim().length < 3 ){
+                    return 'Nome precisa no minimo de 3 letras';
+                  }
+                  return null;
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Preço'),
